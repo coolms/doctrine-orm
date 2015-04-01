e999158515918aa6acc5cb263daa83cd4652e242
Dmitry Popov <d.popov@altgraphic.com>
2015-04-01 10:11:22 -0300

    Initial commit

--- src/Mapping/Common/Traits/DescribableTrait.php
+++ src/Mapping/Common/Traits/DescribableTrait.php
@@ -1,8 +1,8 @@
 <?php
 /**
  * CoolMS2 Doctrine ORM module (http://www.coolms.com/)
- * 
- * @link      http://github.com/coolms/CmsDoctrineORM for the canonical source repository
+ *
+ * @link      http://github.com/coolms/doctrine-orm for the canonical source repository
  * @copyright Copyright (c) 2006-2014 Altgraphic, ALC (http://www.altgraphic.com)
  * @license   http://www.coolms.com/license/new-bsd New BSD License
  * @author    Dmitry Popov <d.popov@altgraphic.com>
